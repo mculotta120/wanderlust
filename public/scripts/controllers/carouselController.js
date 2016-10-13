1d095693014791e81e0c5d087220fdabc1da8010
mculotta120 <margaret.culotta@gmail.com>
2016-10-12 21:46:13 -0500

    carousel started

---
+++ public/scripts/controllers/carouselController.js
@@ -0,0 +1,17 @@
+myApp.controller('carouselController', [ '$scope', '$http', 'ui.bootstrap', function( $scope, $http ){
+  console.log("in carouselController");
+
+    $scope.myInterval = 3000;
+    $scope.slides = [
+      {
+        image: '/images/Issue001_Page001_FrontCover.jpg'
+      },
+      {
+        image: '/images/Issue002_Page001_FrontCover.jpg'
+      },
+      {
+        image: '/images/Issue003_Page001.jpg'
+      }
+    ];
+  }
+]);
--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,3 +1,16 @@
-myApp.controller('homeController', [ '$scope', '$http', function( $scope, $http ){
+myApp.controller('homeController', [ '$scope', '$http', 'ui.bootstrap', function( $scope, $http ){
   console.log("homeController");
+  $scope.myInterval = 3000;
+  $scope.slides = [
+    {
+      image: '/images/Issue001_Page001_FrontCover.jpg'
+    },
+    {
+      image: '/images/Issue002_Page001_FrontCover.jpg'
+    },
+    {
+      image: '/images/Issue003_Page001.jpg'
+    }
+  ];
+
 }]);
